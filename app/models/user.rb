312f351dfd289ce296c20cc4a97659a21aa9a980
Samuel Pérez <fernando.samuelperez01@gmail.com>
2023-07-29 06:19:51 -0600

    Add MCV Tweet

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   
 
   has_many :tweets, dependent: :destroy
-  has_many :likes, dependent: :destroy
+  has_many :likes, dependent: :destroy, counter_cache: true
   has_many :liked_tweets, through: :likes, source: :tweet
   has_many :replies, class_name: 'Tweet', foreign_key: :reply_to_id, dependent: :nullify
   
@@ -13,12 +13,16 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :username, presence: true, uniqueness: true 
   validates :name, presence: true
-  validates :password, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }, if: :password_required?
   enum role: { member: 0, admin: 1 }
   
   private
 
   def default_avatar
-    self.avatar ||= "app/assets/images/default-avatar.png"
+    self.avatar.attach(io: File.open(Rails.root.join('app', 'assets', 'images', 'default-avatar.png')), filename: 'default-avatar.png', content_type: 'image/png') unless self.avatar.attached?
+  end
+
+  def password_required?
+    new_record? || password.present?
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_29_105056) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_29_120411) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,6 +42,19 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_29_105056) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "tweets", force: :cascade do |t|
+    t.text "body", null: false
+    t.integer "replies_count", default: 0
+    t.integer "likes_count", default: 0
+    t.bigint "user_id", null: false
+    t.integer "replied_to"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "replied_to_id", null: false
+    t.index ["replied_to_id"], name: "index_tweets_on_replied_to_id"
+    t.index ["user_id"], name: "index_tweets_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "username"
@@ -50,10 +63,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_29_105056) do
     t.integer "role", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "tweets", "tweets", column: "replied_to_id"
+  add_foreign_key "tweets", "users"
 end
