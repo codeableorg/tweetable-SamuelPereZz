d7a5f5e58b3434424817fb29cc117f3ca7570a01
Samuel Pérez <fernando.samuelperez01@gmail.com>
2023-07-29 08:03:46 -0600

    Add Styles

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,45 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts "Start Seeding"
+
+puts "Seeding Member Users"
+puts "-----------------------"
+puts "User1"
+User.find_or_create_by(username: "menber_test1") do |user|
+  user.email = "member1@test.com"
+  user.username = "User1"
+  user.name = "User1"
+  user.password = "123456"
+end
+
+puts "User2"
+User.find_or_create_by(username: "menber_test2") do |user|
+  user.email = "member2@test.com"
+  user.username = "User2"
+  user.name = "User2"
+  user.password = "123456"
+end
+
+puts "User3"
+User.find_or_create_by(username: "menber_test3") do |user|
+  user.email = "member3@test.com"
+  user.name = "User3"
+  user.name = "User3"
+  user.password = "123456"
+end
+
+puts "User4"
+User.find_or_create_by(username: "menber_test4") do |user|
+  user.email = "member4@test.com"
+  user.username = "User4"
+  user.name = "User4"
+  user.password = "123456"
+end
+
+puts "Seeding Admin User"
+User.find_or_create_by(username: "user_admin") do |user|
+  user.email = "Admin@test.com"
+  user.name = "Admin"
+  user.password = "123456"
+  user.role = "admin"
+end
+
+puts "End Seeding"
