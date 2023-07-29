21e35b4617d0d426abb32c235cd376d32140485f
Samuel Pérez <fernando.samuelperez01@gmail.com>
2023-07-29 15:42:40 -0600

    Modified and implemented the styles in the indexes

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,48 +1,17 @@
-class TweetsController < ApplicationController
-  skip_before_action :authenticate_user!, only: %i[index show]
-  def index
-    @tweets = Tweet.all.order(updated_at: :desc).where(replied_to_id: nil)
-    @tweet_new = Tweet.new
-    @like_new = Like.new
-  end
-
-  def show
-    @tweet_new = Tweet.new
-    @tweet_got = Tweet.find(params[:id])
-  end
-
+class LikesController < ApplicationController
   def create
-    @tweet_new = Tweet.new(tweet_params)
-    @tweet_new.user_id = current_user.id
-    @tweet_new.save
-
-    redirect_to root_path
-  end
-
-  def edit
-    @tweet_got = Tweet.find(params[:id])
-    authorize @tweet_got
-    @tweet_parent = @tweet_got.replied_to
-  end
-
-  def update
-    @tweet_new = Tweet.find(params[:id])
-    authorize @tweet_new
-    @tweet_new.update(tweet_params)
+    @tweet = Tweet.find(params[:tweet])
+    @new_like = Like.new(user: current_user, tweet: @tweet)
+    @new_like.save
 
     redirect_to root_path
   end
 
   def destroy
-    @tweet_new = Tweet.find(params[:id])
-    authorize @tweet_new
-    @tweet_new.destroy
+    @tweet = Tweet.find(params[:id])
+    @like_got = Like.find_by(user: current_user, tweet: @tweet)
+    @like_got.destroy
+
     redirect_to root_path
-  end
-
-  private
-
-  def tweet_params
-    params.require(:tweet).permit(:body, :replied_to_id)
   end
 end
